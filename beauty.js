540afee21196a2c26be6675aa408654261afaee3
kwanngasze <81080759+kwanngasze@users.noreply.github.com>
2021-04-30 22:17:35 +0800

    Add files via upload

---
+++ beauty.js
@@ -0,0 +1,84 @@
+function welcomeAlert() {
+    alert('Welcome to BEAUTY BEAUTY')
+}
+
+function shopnowfunction() {
+    location.href=("Kiehls.html")
+}
+function buyfunction() {
+    location.href=("https://docs.google.com/forms/d/e/1FAIpQLScx_VF3jGetTIbIl3m04sP2ygtG0lep2QYdKOZy9qo50loNlg/viewform?vc=0&c=0&w=1&flr=0&usp=mail_form_link")
+}
+<!-- Kiehl's -->
+function K1() {
+    location.href=("https://shop.kiehls.com.hk/pdp?categoryIds=942&code=HS0855800&lastPathname=%2Fplp&navId=3")
+}
+function K2() {
+    location.href=("https://shop.kiehls.com.hk/pdp?categoryIds=946&code=HS0860600&lastPathname=%2Fplp&navId=7")
+}
+function K3() {
+    location.href=("https://shop.kiehls.com.hk/pdp?categoryIds=946&code=H80126700&lastPathname=%2Fplp&navId=7")
+}
+function K4() {
+    location.href=("https://shop.kiehls.com.hk/pdp?categoryIds=946&code=HS2716200&lastPathname=%2Fplp&navId=7")
+}
+function K5() {
+    location.href=("https://shop.kiehls.com.hk/pdp?categoryIds=946&code=HS2803000&lastPathname=%2Fplp&navId=7")
+}
+function K6() {
+    location.href=("https://shop.kiehls.com.hk/pdp?categoryIds=946&code=HS0400400&lastPathname=%2Fplp&navId=7")
+}
+
+<!-- Estee Lauder -->
+function EL1() {
+    location.href=("https://www.esteelauder.com.hk/en-hk/product/681/77491/product-catalog/skincare/new-advanced-night-repair/synchronized-multi-recovery-complex")
+}
+function EL2() {
+    location.href=("https://www.esteelauder.com.hk/en-hk/product/681/55125/product-catalog/skincare/advanced-night-repair-eye/supercharged-complex-synchronized-recovery")
+}
+function EL3() {
+    location.href=("https://www.esteelauder.com.hk/en-hk/product/681/41721/product-catalog/skincare/revitalizing-supreme/global-anti-aging-power-soft-creme")
+}
+function EL4() {
+    location.href=("https://www.esteelauder.com.hk/en-hk/product/681/64874/product-catalog/skincare/revitalizing-supreme-night/intensive-restorative-creme")
+}
+function EL5() {
+    location.href=("https://www.esteelauder.com.hk/en-hk/product/681/64873/product-catalog/skincare/advanced-night-repair/intense-reset-concentrate")
+}
+function EL6() {
+    location.href=("https://www.esteelauder.com.hk/en-hk/product/684/29363/product-catalog/skincare/cleansers-toners/micro-essence/skin-activating-treatment-lotion")
+}
+
+<!-- Shishedo -->
+function S1() {
+    location.href=("https://www.shiseido.com.hk/en/ultimune-power-infusing-concentrate-1011453430.html?cgid=S2_Category_PowerInfusin")
+}
+function S2() {
+    location.href=("https://www.shiseido.com.hk/en/vital-perfection-white-revitalizing-softener-enriched-1011107680.html?cgid=BestOfShiseido")
+}
+function S3() {
+    location.href=("https://www.shiseido.com.hk/en/shiseido-revitalizing-treatment-softener-1011559510.html")
+}
+function S4() {
+    location.href=("https://www.shiseido.com.hk/en/future-solution-lx-total-regenerating-cream-e-1021392130.html?cgid=BestOfShiseido")
+}
+function S5() {
+    location.href=("https://www.shiseido.com.hk/en/shiseido-men-ultimune-power-infusing-concentrate-1011715330.html?cgid=S2_Collections_Ultimune")
+}
+function S6() {
+    location.href=("https://www.shiseido.com.hk/en/future-solution-lx-intensive-firming-contour-serum-1021392330.html?cgid=BestOfShiseido")
+}
+
+
+<!-- MAC -->
+function M1() {
+    location.href=("https://www.maccosmetics.com.hk/en-hk/product/13854/60284/makeup/lips/lipstick/powder-kiss-lipstick")
+}
+function M2() {
+    location.href=("https://www.maccosmetics.com.hk/en-hk/product/13840/54415/makeup/eyes/shadow/eye-shadow-x-9-burgundy-times-nine")
+}
+function M3() {
+    location.href=("https://www.maccosmetics.com.hk/en-hk/product/13838/64916/makeup/eyes/liner/brushstroke-24-hour-liner")
+}
+function M4() {
+    location.href=("https://www.maccosmetics.com.hk/en-hk/product/13842/329/makeup/face/blush-highlight/powder-blush#!/shade/Melba")
+}
